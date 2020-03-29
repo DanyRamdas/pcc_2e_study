7252dc394703d07343724f1f718b5fdfc10fb774
Dany Ramdas <danyramdas@gmail.com>
2020-03-29 21:17:24 +0200

    add stripping names

---
+++ chapter_02/stripping_names.py
@@ -0,0 +1,4 @@
+famous_person = " Marthe Troly-Curtin "
+famous_quote = '"Time You Enjoy Wasting is Not Wasted Time"  '
+message = f"{famous_person.strip()} once said:\n{famous_quote.strip()}"
+print(message)
